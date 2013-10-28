89cc860b07f3493388ea22d68baa2d57c4e3a607
Gabriel Zea <zea@randomlab.net>
2013-10-28 16:52:21 -0500

    seleccion e inyeccion basica

--- data/snob.js
+++ data/snob.js
@@ -4,8 +4,20 @@ self.port.on("replacePage", function(message){
 
     secciones.each(function(indice, elemento){
         var encabezado = $(this).children("h4");
-
         encabezado.text("hola " + indice );
+
+        var contenido = $(this).children(".texto_section");
+        contenido.each(function(ind, elem){
+            var titulo = $(this).children("h3");
+            var noticia = $(this).children("p");
+
+            titulo.text("una noticia importante");
+            
+            noticia.css({"color": "black"});
+            noticia.text("hsatuoh sasaotuhas as atohuas usaousaonteuha s asntoehusan uaso aonsua usa oeutansuhaoeuasua usa usasua oseuau asu aimportantisima");
+
+
+        })
     }); 
     
 });
