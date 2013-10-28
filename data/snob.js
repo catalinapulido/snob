48fd249a8469fc5e000d08dc18901de8ade65ee5
Gabriel Zea <zea@randomlab.net>
2013-10-28 15:25:24 -0500

    busqueda basica de elementos

--- data/snob.js
+++ data/snob.js
@@ -1,4 +1,11 @@
 //funcion que ejecuta los cambios en la pagina
 self.port.on("replacePage", function(message){
-	$("body").html("<h1>Hola Mundo </h1>");
+	var secciones = $(".section");
+
+    secciones.each(function(indice, elemento){
+        var encabezado = $(this).children("h4");
+
+        encabezado.text("hola " + indice );
+    }); 
+    
 });
